ec6de0d46b9f06608f0e4ffb6254e95c7e66ed23
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-18 23:04:57 +0200

    genetic-algorithm - optimization process
    get all the components of genetic algorithm together in order to optimize given fitness function,
    using population that being changed in each generation,
    either by crossover and genetic-operator, including elitism,
    until some termination-criterion is met

--- src/main/java/optimization/genetic/GeneticAlgorithm.java
+++ src/main/java/optimization/genetic/GeneticAlgorithm.java
@@ -1,10 +1,16 @@
 package optimization.genetic;
 
 import optimization.genetic.operator.GeneticOperator;
+import optimization.genetic.selection.Selector;
 import optimization.genetic.selection.SelectorFactory;
 import termination.TerminationCriterion;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface GeneticAlgorithm<T> {
 	Collection<T> getPopulation();
@@ -28,4 +34,35 @@ public interface GeneticAlgorithm<T> {
 	void incrementGeneration();
 
 	int getElitismSize();
+
+	default Selector<T> run() {
+		Selector<T> selector;
+
+		do {
+			Map<T, Float> fitnessScores = calculateFitnessScores();
+			selector = getSelectorFactory().create(fitnessScores, isMinimization());
+			setPopulation(createNewPopulation(selector));
+			incrementGeneration();
+		} while (!getTerminationCriterion().isDone(this));
+
+		return selector;
+	}
+
+	private Map<T, Float> calculateFitnessScores() {
+		return getPopulation().parallelStream().distinct()
+				.collect(Collectors.toMap(Function.identity(), getFitnessFunction()::evaluate));
+	}
+
+	private List<T> createNewPopulation(Selector<T> selector) {
+		return Stream.of(selector.getElitism(getElitismSize()), createNewOffsprings(selector))
+				.flatMap(Collection::stream)
+				.collect(Collectors.toList());
+	}
+
+	private List<T> createNewOffsprings(Selector<T> selector) {
+		return Stream.generate(() -> getCrossoverFunction().createOffspringBy(selector))
+				.limit(getPopulation().size() - getElitismSize())
+				.map(getGeneticOperator()::apply)
+				.collect(Collectors.toList());
+	}
 }
