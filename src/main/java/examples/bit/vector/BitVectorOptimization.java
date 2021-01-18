c973371fbd5ad44c104ad2e211996540dd0d3e18
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-18 23:16:57 +0200

    genetic-algorithm - use-case simple example
    Create simple example for creating genetic-algorithm to solve the problem of 1-bit vector,
    to maximize the number of bits on.
    Expressed by using Strings for simplicity.

---
+++ src/main/java/examples/bit/vector/BitVectorOptimization.java
@@ -0,0 +1,38 @@
+package examples.bit.vector;
+
+import optimization.genetic.GeneticAlgorithm;
+import optimization.genetic.GeneticAlgorithmBuilder;
+import optimization.genetic.operator.mutation.FixedRateMutation;
+import optimization.genetic.selection.Selector;
+import optimization.genetic.selection.roulette.wheel.RouletteWheelSelector;
+import termination.LimitedEpochsTerminationCriterion;
+
+import java.util.List;
+import java.util.Random;
+
+public class BitVectorOptimization {
+	public static void main(String[] args) {
+		Selector<String> result = new GeneticAlgorithmBuilder<>(List.of("000", "001", "100", "110", "011"))
+				.maximize()
+				.fitnessFunction(str -> (float) str.chars().filter(i -> i == '1').count())
+				.selectorFactory(RouletteWheelSelector::new)
+				.crossoverFunction(selector -> selector.select().charAt(0) + selector.select().substring(1))
+				.elitismSize(1)
+				.geneticOperator(createStringMutation())
+				.terminationCriterion(new LimitedEpochsTerminationCriterion<>(5, GeneticAlgorithm::getGeneration))
+				.build().run();
+
+		result.getElitism(3).forEach(System.out::println);
+	}
+
+	private static FixedRateMutation<String> createStringMutation() {
+		Random random = new Random();
+
+		return new FixedRateMutation<>(0.4,
+				s -> {
+					int randomIndex = random.nextInt(s.length());
+					char flippedChar = s.charAt(randomIndex) == '0' ? '1' : '0';
+					return s.substring(0, randomIndex) + flippedChar + s.substring(randomIndex + 1);
+				});
+	}
+}
