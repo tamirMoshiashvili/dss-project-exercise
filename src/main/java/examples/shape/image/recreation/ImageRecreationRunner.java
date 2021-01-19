d28613f79fd051e5571b962b2ec563638aeec774
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-19 21:47:05 +0200

    extract-to-configurations - image-recreation properties
    use AppProperties, instead of creating each property as a variable

--- src/main/java/examples/shape/image/recreation/ImageRecreationRunner.java
+++ src/main/java/examples/shape/image/recreation/ImageRecreationRunner.java
@@ -7,7 +7,6 @@ import examples.shape.image.recreation.utils.ShapeSpecificationGenerator;
 import image.recreation.ImageRecreation;
 import image.recreation.shape.ShapeImageRecreation;
 import image.recreation.shape.ShapeSpecification;
-import image.recreation.shape.conversion.EllipseShapeConversionFunction;
 import image.recreation.shape.conversion.ShapeConversionFunction;
 import lombok.extern.slf4j.Slf4j;
 import optimization.genetic.CrossoverFunction;
@@ -18,7 +17,6 @@ import optimization.genetic.operator.GeneticOperator;
 import optimization.genetic.operator.mutation.FixedRateMutation;
 import optimization.genetic.selection.roulette.wheel.RouletteWheelSelector;
 import termination.LimitedTimeTerminationCriterion;
-import utils.ImageUtils;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -32,35 +30,32 @@ import static image.loss.BufferedImageMSE.calculateLoss;
 @Slf4j
 public class ImageRecreationRunner {
 	public static void main(String[] args) {
-		BufferedImage targetImage = ImageUtils.loadImage("images\\mona-lisa-1.png");
-		createGeneticAlgorithm(targetImage).run();
+		AppProperties properties = new AppProperties();
+		createGeneticAlgorithm(properties).run();
 	}
 
-	private static GeneticAlgorithm<ImageRecreation<ShapeSpecification>> createGeneticAlgorithm(BufferedImage targetImage) {
-		int populationSize = 32;
-		int width = targetImage.getWidth();
-		int height = targetImage.getHeight();
-		int initialSize = 1;
-		int elitismSize = 4;
-		int delta = 1;
-		int threshold = 100;
-		double mutationRate = 0.5;
-		ShapeConversionFunction<? extends Shape> shapeConversionFunction = new EllipseShapeConversionFunction();
+	private static GeneticAlgorithm<ImageRecreation<ShapeSpecification>> createGeneticAlgorithm(AppProperties properties) {
+		BufferedImage targetImage = properties.getTargetImage();
 
-		return new GeneticAlgorithmBuilder<>(createInitialPopulation(populationSize, width, height, initialSize, shapeConversionFunction))
+		return new GeneticAlgorithmBuilder<>(createInitialPopulation(properties))
 				.minimize()
-				.fitnessFunction(individual -> calculateLoss(targetImage, individual.recreateImage(width, height)))
+				.fitnessFunction(individual -> calculateLoss(targetImage, individual.recreateImage(targetImage.getWidth(), targetImage.getHeight())))
 				.selectorFactory(RouletteWheelSelector::new)
-				.crossoverFunction(getCrossoverFunction(shapeConversionFunction))
-				.elitismSize(elitismSize)
-				.geneticOperator(getMutation(width, height, delta, threshold, mutationRate, shapeConversionFunction))
+				.crossoverFunction(getCrossoverFunction(properties.getShapeConversionFunction()))
+				.elitismSize(properties.getElitismSize())
+				.geneticOperator(getMutation(properties))
 				.terminationCriterion(new LimitedTimeTerminationCriterion<>(Duration.ofMinutes(5)))
 				.generationListeners(List.of(new StatisticsCollector<>(), new ShapeImageRecreationDrawer(targetImage)))
 				.build();
 	}
 
-	private static List<ImageRecreation<ShapeSpecification>> createInitialPopulation(
-			int size, int width, int height, int initialSize, ShapeConversionFunction<? extends Shape> shapeConversionFunction) {
+	private static List<ImageRecreation<ShapeSpecification>> createInitialPopulation(AppProperties properties) {
+		int width = properties.getTargetImage().getWidth();
+		int height = properties.getTargetImage().getHeight();
+		int initialSize = properties.getInitialShapeSpecificationsSize();
+		ShapeConversionFunction<? extends Shape> shapeConversionFunction = properties.getShapeConversionFunction();
+		int size = properties.getPopulationSize();
+
 		return Stream.generate(() ->
 				new ShapeImageRecreation<>(ShapeSpecificationGenerator.createRandomShapeSpecifications(width, height, initialSize), shapeConversionFunction))
 				.limit(size)
@@ -73,10 +68,13 @@ public class ImageRecreationRunner {
 				new ShapeImageRecreation<>(shapeSpecifications, shapeConversionFunction));
 	}
 
-	private static FixedRateMutation<ImageRecreation<ShapeSpecification>> getMutation(
-			int width, int height, int delta, int threshold, double mutationRate, ShapeConversionFunction<? extends Shape> shapeConversionFunction) {
+	private static FixedRateMutation<ImageRecreation<ShapeSpecification>> getMutation(AppProperties properties) {
 		GeneticOperator<ImageRecreation<ShapeSpecification>> geneticOperator =
-				new AdditiveShapeSpecificationOperator<>(width, height, threshold, delta, shapeConversionFunction);
-		return new FixedRateMutation<>(mutationRate, geneticOperator);
+				new AdditiveShapeSpecificationOperator<>(properties.getTargetImage().getWidth(),
+						properties.getTargetImage().getHeight(),
+						properties.getShapeSpecificationThreshold(),
+						properties.getShapeSpecificationDelta(),
+						properties.getShapeConversionFunction());
+		return new FixedRateMutation<>(properties.getMutationRate(), geneticOperator);
 	}
 }
