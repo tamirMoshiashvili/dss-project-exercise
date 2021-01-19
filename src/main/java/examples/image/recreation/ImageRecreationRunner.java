86d5e610a2940743f7e04bb9f6c352e21ced0784
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-19 19:29:41 +0200

    ga-shape-image-recreation - main runner
    create genetic-algorithm to recreate mona-lisa image

---
+++ src/main/java/examples/image/recreation/ImageRecreationRunner.java
@@ -0,0 +1,81 @@
+package examples.image.recreation;
+
+import examples.image.recreation.shape.genetic.AdditiveShapeSpecificationOperator;
+import examples.image.recreation.shape.genetic.ShapeImageRecreationCrossoverFunction;
+import image.recreation.ImageRecreation;
+import image.recreation.shape.ShapeImageRecreation;
+import image.recreation.shape.ShapeSpecification;
+import image.recreation.shape.conversion.EllipseShapeConversionFunction;
+import image.recreation.shape.conversion.ShapeConversionFunction;
+import lombok.extern.slf4j.Slf4j;
+import optimization.genetic.CrossoverFunction;
+import optimization.genetic.GeneticAlgorithm;
+import optimization.genetic.GeneticAlgorithmBuilder;
+import optimization.genetic.listener.StatisticsCollector;
+import optimization.genetic.operator.GeneticOperator;
+import optimization.genetic.operator.mutation.FixedRateMutation;
+import optimization.genetic.selection.roulette.wheel.RouletteWheelSelector;
+import termination.LimitedTimeTerminationCriterion;
+import utils.ImageUtils;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static examples.image.recreation.shape.utils.ShapeSpecificationGenerator.createRandomShapeSpecifications;
+import static image.loss.BufferedImageMSE.calculateLoss;
+
+@Slf4j
+public class ImageRecreationRunner {
+	public static void main(String[] args) {
+		BufferedImage targetImage = ImageUtils.loadImage("images\\mona-lisa-1.png");
+		createGeneticAlgorithm(targetImage).run();
+	}
+
+	private static GeneticAlgorithm<ImageRecreation<ShapeSpecification>> createGeneticAlgorithm(BufferedImage targetImage) {
+		int populationSize = 32;
+		int width = targetImage.getWidth();
+		int height = targetImage.getHeight();
+		int initialSize = 1;
+		int elitismSize = 4;
+		int delta = 1;
+		int threshold = 100;
+		double mutationRate = 0.5;
+		ShapeConversionFunction<? extends Shape> shapeConversionFunction = new EllipseShapeConversionFunction();
+
+		return new GeneticAlgorithmBuilder<>(createInitialPopulation(populationSize, width, height, initialSize, shapeConversionFunction))
+				.minimize()
+				.fitnessFunction(individual -> calculateLoss(targetImage, individual.recreateImage(width, height)))
+				.selectorFactory(RouletteWheelSelector::new)
+				.crossoverFunction(getCrossoverFunction(shapeConversionFunction))
+				.elitismSize(elitismSize)
+				.geneticOperator(getMutation(width, height, delta, threshold, mutationRate, shapeConversionFunction))
+				.terminationCriterion(new LimitedTimeTerminationCriterion<>(Duration.ofMinutes(5)))
+				.generationListeners(List.of(new StatisticsCollector<>()))
+				.build();
+	}
+
+	private static List<ImageRecreation<ShapeSpecification>> createInitialPopulation(
+			int size, int width, int height, int initialSize, ShapeConversionFunction<? extends Shape> shapeConversionFunction) {
+		return Stream.generate(() ->
+				new ShapeImageRecreation<>(createRandomShapeSpecifications(width, height, initialSize), shapeConversionFunction))
+				.limit(size)
+				.collect(Collectors.toList());
+	}
+
+	private static CrossoverFunction<ImageRecreation<ShapeSpecification>> getCrossoverFunction(
+			ShapeConversionFunction<? extends Shape> shapeConversionFunction) {
+		return new ShapeImageRecreationCrossoverFunction(shapeSpecifications ->
+				new ShapeImageRecreation<>(shapeSpecifications, shapeConversionFunction));
+	}
+
+	private static FixedRateMutation<ImageRecreation<ShapeSpecification>> getMutation(
+			int width, int height, int delta, int threshold, double mutationRate, ShapeConversionFunction<? extends Shape> shapeConversionFunction) {
+		GeneticOperator<ImageRecreation<ShapeSpecification>> geneticOperator =
+				new AdditiveShapeSpecificationOperator<>(width, height, threshold, delta, shapeConversionFunction);
+		return new FixedRateMutation<>(mutationRate, geneticOperator);
+	}
+}
