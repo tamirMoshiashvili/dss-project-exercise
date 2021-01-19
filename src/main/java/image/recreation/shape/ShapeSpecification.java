37fc33c532311418352ff39229d68c0ea24e8f77
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-19 18:54:25 +0200

    shape-image-recreation - refactor
    replace getters & setters with lombok-annotation

--- src/main/java/image/recreation/shape/ShapeSpecification.java
+++ src/main/java/image/recreation/shape/ShapeSpecification.java
@@ -1,8 +1,11 @@
 package image.recreation.shape;
 
+import lombok.Data;
+
 import java.awt.*;
 import java.awt.geom.Point2D;
 
+@Data
 public class ShapeSpecification {
 	private Point2D.Float location;
 	private int orientation;
@@ -13,38 +16,6 @@ public class ShapeSpecification {
 		this.location = location;
 		this.orientation = orientation;
 		this.scale = scale;
-		this.color = color;
-	}
-
-	public Point2D.Float getLocation() {
-		return location;
-	}
-
-	public void setLocation(Point2D.Float location) {
-		this.location = location;
-	}
-
-	public int getOrientation() {
-		return orientation;
-	}
-
-	public void setOrientation(int orientation) {
-		this.orientation = orientation;
-	}
-
-	public float getScale() {
-		return scale;
-	}
-
-	public void setScale(float scale) {
-		this.scale = scale;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void setColor(Color color) {
 		this.color = color;
 	}
 
