8bea73911f37e1fd3b517818580d4bcad23a66d1
Tamir <Tamir@DESKTOP-8R67EMV>
2021-01-23 19:39:09 +0200

    refactor-rename-properties - extract optimization properties
    extract optimization-properties from genetic-algorithm-properties

--- src/main/java/config/genetic/algorithm/GeneticAlgorithmConfig.java
+++ src/main/java/config/genetic/algorithm/GeneticAlgorithmConfig.java
@@ -4,8 +4,6 @@ import lombok.Data;
 
 @Data
 public class GeneticAlgorithmConfig {
-	private int populationSize;
 	private MutationConfig mutation;
 	private int elitismSize;
-	private TerminationConfig termination;
 }
